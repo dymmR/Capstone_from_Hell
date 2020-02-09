260d3acced2a894fb271319690c922fbe356589e
dymmR <liammail35@gmail.com>
2020-02-09 15:46:05 -0700

    hi robbie

---
+++ app/src/main/java/com/example/attempt_feb6/MainActivity.kt
@@ -0,0 +1,133 @@
+package com.example.attempt_feb6
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import java.util.*
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.util.Log
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.io.IOException
+import android.Manifest
+import android.widget.EditText
+import android.view.View
+import android.content.pm.PackageManager
+
+class MainActivity : AppCompatActivity() {
+    val textview=findViewById<TextView>(R.id.textView)
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        BluetoothServerController(this, textview).start()
+
+    }
+
+    val uuid: UUID= UUID.fromString("8989063a-c9af-463a-b3f1-f21d9b2b827b")
+
+    class BluetoothServerController(activity: MainActivity, textview: TextView) : Thread() {
+        private var cancelled: Boolean
+        private val serverSocket: BluetoothServerSocket?
+        private val activity = activity
+        val uuid: UUID= UUID.fromString("8989063a-c9af-463a-b3f1-f21d9b2b827b")
+        val textV : TextView = textview
+
+
+
+        init {
+            val btAdapter = BluetoothAdapter.getDefaultAdapter()
+            if (btAdapter != null) {
+                this.serverSocket = btAdapter.listenUsingRfcommWithServiceRecord("test", uuid) // 1
+                this.cancelled = false
+            } else {
+                this.serverSocket = null
+                this.cancelled = true
+            }
+
+        }
+
+
+        override fun run() {
+            var socket: BluetoothSocket
+
+            while(true) {
+                if (this.cancelled) {
+                    break
+                }
+
+                try {
+                    socket = serverSocket!!.accept()  // 2
+                } catch(e: IOException) {
+                    break
+                }
+
+                if (!this.cancelled && socket != null) {
+                    Log.i("server", "Connecting")
+                    BluetoothServer(this.activity, socket, textV).start() // 3
+                }
+            }
+        }
+
+        fun cancel() {
+            this.cancelled = true
+            this.serverSocket!!.close()
+        }
+    }
+
+
+
+    class BluetoothServer(private val activity: MainActivity, private val socket: BluetoothSocket, private val textview : TextView): Thread() {
+        private val inputStream = this.socket.inputStream
+        private val outputStream = this.socket.outputStream
+        val textV : TextView = textview
+
+
+        override fun run() {
+            try {
+                val available = inputStream.available()
+                val bytes = ByteArray(available)
+                Log.i("server", "Reading")
+                inputStream.read(bytes, 0, available)
+                val text = String(bytes)
+                Log.i("server", "Message received")
+                Log.i("server", text)
+
+                textV.text = text
+                //activity.appendText(text)
+            } catch (e: Exception) {
+                Log.e("client", "Cannot read data", e)
+            } finally {
+                inputStream.close()
+                outputStream.close()
+                socket.close()
+            }
+        }
+    }
+
+
+
+}
